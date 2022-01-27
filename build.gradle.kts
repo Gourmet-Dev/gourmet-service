36532925d061e58619b5e2e0e728ef244ede8811
Hyeonho Seo <seohho@gmail.com>
2022-01-28 00:00:02 +0900

    topic-ms-00001: Gradle기반의 프로젝트를 구성합니다.

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.6.10"
+}
+
+group = "com.gourmet"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
