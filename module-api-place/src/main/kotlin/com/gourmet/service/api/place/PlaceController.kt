4ece66149238a02d5f6ab0bb4fc934d3557e3836
Hyeonho Seo <seohho@gmail.com>
2022-02-02 02:21:53 +0900

    topic-ms-00001: 'module-api-place' 기능 개발.

--- module-api-place/src/main/kotlin/com/gourmet/service/api/place/PlaceController.kt
+++ module-api-place/src/main/kotlin/com/gourmet/service/api/place/PlaceController.kt
@@ -1,4 +1,19 @@
 package com.gourmet.service.api.place
 
-class PlaceController {
+import com.gourmet.service.api.place.payload.GetAllPlacesResponse
+import com.gourmet.service.core.place.usecase.PlaceService
+import org.springframework.http.MediaType.APPLICATION_NDJSON_VALUE
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
+
+@RestController
+@RequestMapping("/place")
+class PlaceController(private val service: PlaceService) {
+    @GetMapping("/", produces = [APPLICATION_NDJSON_VALUE])
+    fun getAllPlaces(): Flux<GetAllPlacesResponse> {
+        return service.getAllPlaces()
+            .map { GetAllPlacesResponse.fromData(it) }
+    }
 }
